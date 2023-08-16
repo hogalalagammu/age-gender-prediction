619c4d76d6062ec82b9b0dd19771465046d6fad9
hogalalagammu <118367654+hogalalagammu@users.noreply.github.com>
2023-08-16 20:26:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)
 genderList = ['Male', 'Female']
 
 
-video=cv2.VideoCapture('4.mp4')
+video=cv2.VideoCapture(0)
 
 padding=20
 
